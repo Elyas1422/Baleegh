eb740602ad0d74252916dab3c692ca3a7ce4edd9
Elyas Almubarak <79568124+Elyas1422@users.noreply.github.com>
2024-11-02 17:51:18 +0300

    Refactor footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { TanslationSectionComponent } from './components/tanslation-section/tanslation-section.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, TanslationSectionComponent],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    TanslationSectionComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
